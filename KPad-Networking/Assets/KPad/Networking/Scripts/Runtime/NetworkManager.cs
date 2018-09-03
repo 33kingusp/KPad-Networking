b28c173d2a20b5135b4ffe6f0939a63f9b290380
DeskTop_33kingusp <33kingusp@gmail.com>
2018-09-03 11:07:59 +0900

    スクリプトファイルの追加

---
+++ KPad-Networking/Assets/KPad/Networking/Scripts/Runtime/NetworkManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KPad.Networking
+{
+    public class NetworkManager : UnityEngine.Networking.NetworkManager
+    {
+
+    }
+}
---
+++ KPad-Networking/Assets/KPad/Networking/Scripts/Runtime/NetworkSpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KPad.Networking
+{
+    public class NetworkSpawnManager : MonoBehaviour
+    {
+        [SerializeField] private List<GameObject> spawnObjects = new List<GameObject>();
+
+        private void Awake()
+        {
+            foreach (GameObject gameObject in spawnObjects)
+            {
+                if (gameObject == null) continue;
+                UnityEngine.Networking.ClientScene.RegisterPrefab(gameObject);
+            }
+        }
+    }
+}
